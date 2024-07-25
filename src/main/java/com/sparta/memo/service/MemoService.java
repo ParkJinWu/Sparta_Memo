2ae2dabb4da1e7ce86b3c1a28e9f8bd129a9f22b
ParkJinWu <onu0311@gmail.com>
2024-07-25 18:24:25 +0900

    Spring boot의 JPA

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -9,10 +9,13 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class MemoService {
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoRepository memoRepository;
+    //private final JdbcTemplate jdbcTemplate;
 
+    //method가 호출이 될 때마다 만들지 않기 위해 MemoService가 생성자를 통해서
+    // 생성이 될 때 파라미터로 jdbcTemplate받아오고 memoRepository를 만듦
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository = new MemoRepository(jdbcTemplate);
     }
 
 
@@ -22,7 +25,6 @@ public class MemoService {
         Memo memo = new Memo(requestDto);
 
         // DB 저장
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         Memo saveMemo = memoRepository.save(memo); // memo클래스 객체 하나 == DB한줄 ❗️ ➡️ 한 줄 저장
 
         // Entity -> ResponseDto
@@ -33,12 +35,10 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
         //DB 조회
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         return memoRepository.findAll();
     }
 
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
@@ -54,7 +54,6 @@ public class MemoService {
 
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         // 해당 메모가 DB에 존재하는지 확인
         Memo memo = memoRepository.findById(id);
         if(memo != null) {
